31088cfb92567d60c0456e48f69f0de1bbfbe82d
Alexandre Bortolozo <alexandre7.bortolozo@gmail.com>
2024-03-27 20:37:37 -0300

    feat: implements-consultas

--- src/main/java/br/com/fatecrestapi/FoodFatec/repository/CustomerRepository.java
+++ src/main/java/br/com/fatecrestapi/FoodFatec/repository/CustomerRepository.java
@@ -1,9 +1,23 @@
 package br.com.fatecrestapi.FoodFatec.repository;
 
 import br.com.fatecrestapi.FoodFatec.entity.Customer;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+    Optional<Customer> findByCpfCustomer(String cpf);
+
+    Optional<Customer> findByEmailCustomer(String email);
+
+    @Query(value = "SELECT c.* FROM c WHERE c.date_created_customer >= ?;", nativeQuery =true)
+    List<Customer> findByDateCreate(@Param("dateCreatedCustomer")LocalDate dateCreatedCustomer);
 
 
 
