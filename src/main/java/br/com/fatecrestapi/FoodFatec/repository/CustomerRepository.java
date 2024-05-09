bef1a5ed08fe429ae08cbe40f05bc959142b71d0
Alexandre Bortolozo <alexandre7.bortolozo@gmail.com>
2024-05-08 21:02:32 -0300

    feat: implements response generic exception

--- src/main/java/br/com/fatecrestapi/FoodFatec/repository/CustomerRepository.java
+++ src/main/java/br/com/fatecrestapi/FoodFatec/repository/CustomerRepository.java
@@ -1,7 +1,6 @@
 package br.com.fatecrestapi.FoodFatec.repository;
 
 import br.com.fatecrestapi.FoodFatec.entity.Customer;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,13 +11,11 @@ import java.util.Optional;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
-    Optional<Customer> findByCpfCustomer(String cpf);
+    Optional<Customer> findByCpfCustomer(String cpfCustomer);
+    Optional<Customer> findByEmailCustomer(String emailCustomer);
+    List<Customer> findByStatusCustomerIsTrueOrderByDateCreatedCustomerDesc();
 
-    Optional<Customer> findByEmailCustomer(String email);
-
-    @Query(value = "SELECT c.* FROM c WHERE c.date_created_customer >= ?;", nativeQuery =true)
-    List<Customer> findByDateCreate(@Param("dateCreatedCustomer")LocalDate dateCreatedCustomer);
-
-
+    @Query(value = "SELECT c.* FROM customer c WHERE c.date_created_customer >= ?;", nativeQuery = true)
+    List<Customer> findByDateCreated(@Param("dateCreatedCustomer") LocalDate dateCreatedCustomer);
 
 }
